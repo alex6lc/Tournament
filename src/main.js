91f23398bfb10a1e5223e45f31f1fa0134eb7208
Alexis L-c <alexisl-c@hotmail.com>
2014-02-26 22:36:23 -0500

    Fixed tabulation in main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ require.config({
         'handlebars': {
             exports: 'Handlebars'
         },
-		'jqueryui': {
-			deps: ['jquery']
-		}
+        'jqueryui': {
+            deps: ['jquery']
+        }
     },
     paths: {
         'jquery': '3rd/jquery/dist/jquery',
@@ -13,8 +13,8 @@ require.config({
         'underscore': '3rd/underscore/underscore',
         'backbone': '3rd/backbone/backbone',
         'marionette': '3rd/marionette/lib/core/amd/backbone.marionette',
-		'backbone.wreqr': '3rd/backbone.wreqr/lib/amd/backbone.wreqr',
-		'backbone.babysitter':  '3rd/backbone.babysitter/lib/amd/backbone.babysitter',
+        'backbone.wreqr': '3rd/backbone.wreqr/lib/amd/backbone.wreqr',
+        'backbone.babysitter':  '3rd/backbone.babysitter/lib/amd/backbone.babysitter',
         'hbs': '3rd/hbs/hbs',
         'handlebars': '3rd/handlebars/handlebars',
         'localstorage': '3rd/backbone.localStorage/backbone.localStorage',
