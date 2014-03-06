1a69313e09c862918ad59fc4b912550269445a55
Alexis L-c <alexisl-c@hotmail.com>
2014-03-05 23:12:23 -0500

    Add small navigation and enable hreh pushstate

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ define(['jquery', 'marionette', 'backbone', 'router'], function ($, Marionette, 
     var app = new Marionette.Application();
 
     app.addInitializer(function(){
-        var router = new Router({ app: app });
+        var router = app.router = new Router({ app: app });
 
         router.on("route", function(page, args) {
             if (args.length > 0 && args[0] !== null) {
@@ -26,6 +26,16 @@ define(['jquery', 'marionette', 'backbone', 'router'], function ($, Marionette, 
     app.on("initialize:after", function () {
         if (Backbone.history) {
             Backbone.history.start({ pushState: true });
+
+            $(document).on('click', 'a:not([data-bypass])', function (event) {
+                var href = $(this).attr('href');
+                var protocol = this.protocol + '//';
+
+                if (href.slice(protocol.length) !== protocol) {
+                    event.preventDefault();
+                    app.router.navigate(href, true);
+                }
+            });
         }
     });
 
