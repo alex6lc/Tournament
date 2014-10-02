e67a48dc968331d4b8e51b2600e4e5a177fff346
Alexis Leroux-Chartre <alexis.leroux.chartre@gmail.com>
2014-10-01 22:14:41 -0400

    New files for double elimination

---
+++ src/scripts/generator/stages/editor/elimination/double-bracket-view.js
@@ -0,0 +1,79 @@
+define([
+    'underscore',
+    'marionette',
+    'iscroll',
+    'tournaments/stages/elimination/bracket-config',
+    'generator/stages/editor/elimination/round-view',
+    'hbs!generator/stages/editor/elimination/bracket-tmp'
+], function (_, Marionette, IScroll, Config, RoundView, Template) {
+    'use strict';
+
+    return  Marionette.CompositeView.extend({
+        className: 'clearfix bracket',
+        template: Template,
+        childViewContainer: '.viewport-content',
+        childView: RoundView,
+        childViewOptions: function () {
+            return {
+                bracketPos: this.bracketPos,
+                participants: this.participants
+            };
+        },
+
+        initialize: function (options) {
+            this.participants = options.participants;
+            this.bracketPos = this.calculateBracketPos();
+        },
+
+        bindParticipants: function () {
+            this.children.each(function (view) {
+                view.bindParticipants();
+            });
+        },
+
+        renderParticipants: function () {
+            this.children.each(function (view) {
+                view.renderParticipants();
+            });
+        },
+
+        onRender: function () {
+            var maxMatches = this.collection.first().getMatches().length;
+            var totalHeight = maxMatches * (Config.heightMatch + Config.spacerHori) + Config.height + Config.spacerHori;
+            var w = this.collection.length * (Config.widthMatch + Config.spacerVerc) - Config.spacerVerc;
+
+            this.$(".viewport").width(w);
+            this.$(".viewport").height(totalHeight);
+        },
+
+        onDomRefresh: function () {
+            /*this.myScroll = new IScroll(this.$el[0], {
+                zoom: true,
+                zoomMax: 1,
+                zoomMin: 0.5,
+                scrollX: true,
+                scrollY: true,
+                mouseWheel: true,
+                wheelAction: 'zoom'
+            });*/
+        },
+
+        calculateBracketPos: function () {
+            var pos = [];
+            this.collection.each(function (round, rIndex) {
+                var newPos = [];
+                _.each(round.getMatches(), function (match, mIndex) {
+                    if (rIndex === 0) {
+                        newPos.push(mIndex * (Config.heightMatch + Config.spacerHori) + Config.height + Config.spacerHori);
+                    } else {
+                        var rPos = pos[rIndex - 1];
+                        newPos.push(Math.floor((rPos[mIndex * 2] + rPos[(mIndex * 2) + 1]) / 2));
+                    }
+                });
+                pos.push(newPos);
+            });
+
+            return pos;
+        }
+    });
+});
